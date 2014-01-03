62c1f77d61c871fb29baab8a05b097784969f7b8
Ferenc Grecu <mivor20@gmail.com>
2014-01-03 22:44:16 +0200

    Add lib stub

---
+++ TestsCalcNum/TestContFunc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LibCalcNum;
+
+namespace TestsCalcNum
+{
+    [TestFixture]
+    class TestContFunc
+    {
+        [Test]
+        public void DiferenteDivizate()
+        {
+
+        }
+    }
+}
