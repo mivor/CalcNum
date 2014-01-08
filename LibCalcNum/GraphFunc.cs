7e7141ffedf33c5a7601d38ec2daf4d16c25f33a
Ferenc Grecu <mivor20@gmail.com>
2014-01-08 06:58:26 +0200

    Spike of GraphFunction

--- LibCalcNum/GraphFunc.cs
+++ LibCalcNum/GraphFunc.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace LibCalcNum
 {
-    class GraphFunc
+    public class GraphFunction
     {
+        public RegLine RegressionLine { get; private set; }
+        public List<Node> Nodes { get; private set; }
+
+        public GraphFunction(List<Node> pNodes)
+        {
+            Nodes = new List<Node>(pNodes);
+        }
+
+        public GraphFunction(double[] pNodes, double[] pValNodes)
+        {
+            Nodes = Node.ArrayToNodes(pNodes, pValNodes);
+        }
+
+        public void GetRegressionLine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class RegLine
+        {
+            public double A { get; private set; }
+            public double B { get; private set; }
+
+            public RegLine(double a, double b)
+            {
+                this.A = a;
+                this.B = b;
+            }
+        }
+
+        public class RegParabola : RegLine
+        {
+            public double C { get; private set; }
+
+            public RegParabola(double a, double b, double c)
+                : base(a, b)
+            {
+                this.C = c;
+            }
+        }
     }
 }
--- TestsCalcNum/TestGraphFunc.cs
+++ TestsCalcNum/TestGraphFunc.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using LibCalcNum;
 
 namespace TestsCalcNum
 {
+    [TestFixture]
     class TestGraphFunc
     {
+        double[] nod;
+        double[] valNod;
+        GraphFunction graph;
+
+        [Test]
+        public void RegressionLine()
+        {
+            GraphFunction.RegLine result = new GraphFunction.RegLine(95 / 281, 399 / 281);
+            nod = new double[] { 1, 3, 4, 6, 8, 9 };
+            valNod = new double[] { 1, 2, 4, 4, 5, 3};
+            graph = new GraphFunction(nod, valNod);
+
+            graph.GetRegressionLine();
+
+            Assert.That(graph.RegressionLine, Is.EqualTo(result));
+        }
+
     }
 }
