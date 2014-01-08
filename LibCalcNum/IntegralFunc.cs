a6d47d20bb02f50ef7020f923ac20637322ab812
Ferenc Grecu <mivor20@gmail.com>
2014-01-08 08:39:35 +0200

    Spike out IntegralFunc

---
+++ LibCalcNum/IntegralFunc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibCalcNum
+{
+    public class IntegralFunc
+    {
+        public void TrapezoidRomberg(double x, Func<double, double> function)
+        {
+
+        }
+    }
+}
--- TestsCalcNum/TestGraphFunc.cs
+++ TestsCalcNum/TestGraphFunc.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LibCalcNum;
 using NUnit.Framework;
-using LibCalcNum;
+using System.Collections.Generic;
 
 namespace TestsCalcNum
 {
