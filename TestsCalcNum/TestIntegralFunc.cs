706c05192ef3f8e5a08951e0dc111ff2de57ce0e
Ferenc Grecu <mivor20@gmail.com>
2014-01-08 10:34:13 +0200

    Test for EulerMacLaurin

--- TestsCalcNum/TestIntegralFunc.cs
+++ TestsCalcNum/TestIntegralFunc.cs
@@ -40,5 +40,20 @@ namespace TestsCalcNum
 
             Assert.That(integral.Solution, Is.EqualTo(result).Within(maxErr));
         }
+
+        [Test]
+        public void EulerMacLaurin()
+        {
+            double limitA = 0;
+            double limitB = 1;
+            double evaluations = 5025;
+            double maxErr = 0.0001;
+            double result = Math.Log(2);
+            IntegralFunc integral = new IntegralFunc(x => 1 / (x + 1), limitA, limitB);
+
+            integral.AproxEulerMacLaurin(evaluations);
+
+            Assert.That(integral.Solution, Is.EqualTo(result).Within(maxErr));
+        }
     }
 }
