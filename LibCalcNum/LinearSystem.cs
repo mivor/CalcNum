9ace637711cdddae23bcacbf998ede421f4dfce8
Ferenc Grecu <mivor20@gmail.com>
2014-01-15 10:29:42 +0200

    Add stub and test for linear systems gauss

---
+++ LibCalcNum/LinearSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibCalcNum
+{
+    public class LinearSystem
+    {
+
+        public double[] Solution { get; private set; }
+        public double[,] Matrix { get; private set; }
+        public double[] ConsTerms { get; private set; }
+        public int MatrixOrder { get; private set; }
+
+
+        public LinearSystem(double[,] matrix, double[] consTerms)
+        {
+            this.Matrix = matrix;
+            this.ConsTerms = consTerms;
+            this.MatrixOrder = matrix.GetLength(0) > matrix.GetLength(1) 
+                ? matrix.GetLength(0) : matrix.GetLength(1);
+        }
+        public void ResolveGauss()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ TestsCalcNum/TestLinearSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LibCalcNum;
+
+namespace TestsCalcNum
+{
+    [TestFixture]
+    class TestLinearSystem
+    {
+        [Test]
+        public void Gauss()
+        {
+            //int ord = 3;
+            double[,] matrix = new double[3, 3] { {2, 2, 2}, {1, -1, 0}, {3, 1, 2}};
+            double[] constTerms = new double[3] {6, 2, 8};
+            double[] result = new double[3] {2, 0, 1};
+
+            LinearSystem system = new LinearSystem(matrix, constTerms);
+
+            system.ResolveGauss();
+
+            Assert.That(system.Solution, Is.EqualTo(result));
+        }
+    }
+}
